5ebb8d9f89233ee64ac64d10c825ec4f529d4971
IntionLee <intion110383@gmail.com>
2015-12-17 21:35:39 +0800

    Update and rename newClient.java to Client.java

--- Client.java
+++ Client.java
@@ -8,17 +8,6 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-
 
 class Client {
     private Socket socket;
@@ -26,6 +15,7 @@ class Client {
     private DataInputStream input_;
     private String message;
     private String sender;
+    private String clientname;
     private ArrayList<String> userlist;
 
     /*constructor of Client
@@ -37,6 +27,7 @@ class Client {
             input_ = new DataInputStream(socket.getInputStream());
             message = "";
             sender = "";
+            clientname= "";
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -68,6 +59,18 @@ class Client {
         }
     }
 
+    /*send a clientname request to Server*/
+    public void requestClientname() {
+        try {
+            output_.writeUTF("request:clientname");
+            output_.flush();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     /*output to the other user, this is a p2p output
       when call this function, you have to give who you want to send
       and what you want to send*/
@@ -87,7 +90,8 @@ class Client {
      it need to use function getmessage() and getSender() and getUserlist()
      if return 1, the Server send a chatroom message
      if return 2, the Server send a p2p message
-     if return 3, the Server send a online user list*/
+     if return 3, the Server send a online user list
+     if return 4, the Server send a clientname of this client*/
     //public String input() {
     public int input() {
         int flag = 0;
@@ -138,7 +142,11 @@ class Client {
                 }
                 message = str.replace(sender,"");
                 flag = 2;
-            }
+            } else if (str.startsWith("request:clientname")) {
+                /*get clientname, store it as clientname*/
+                clientname = str.replace("request:clientname","");
+                flag = 4;
+            } 
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -163,6 +171,11 @@ class Client {
         return userlist;
     }
 
+    /*return the clientname of this Client*/
+    public String getClientname() {
+        return clientname;
+    }
+
     /*close connection*/
     public void close() {
         try {
@@ -174,85 +187,5 @@ class Client {
         } catch (IOException e) {
             e.printStackTrace();
         }	
-    }
-}
-
-/*this is a test UI, it can be replaced*/
-public class newClient extends JFrame{
-    private JPanel p= new JPanel();
-    private JTextField jtf1 = new JTextField("content");
-    private JTextField jtf2 = new JTextField("to");
-    private JTextArea jta1 = new JTextArea();
-    private JButton button  = new JButton("发送");
-    private JButton button2  = new JButton("发送P2P");
-    private JButton button3  = new JButton("UserList");
-    private Client client;
-    public newClient(){
-        p.setLayout(new BorderLayout());  
-        JPanel p2 = new JPanel();
-        p2.setLayout(new BorderLayout());   
-        p2.add(jtf1,BorderLayout.CENTER);
-	p2.add(jtf2,BorderLayout.NORTH);
-        p2.add((new JLabel("chatroom")),BorderLayout.SOUTH);
-        p2.add(button,BorderLayout.EAST);
-	p2.add(button2,BorderLayout.WEST);
-        p2.add(button3,BorderLayout.SOUTH);
-        p.add(p2,BorderLayout.SOUTH);
-        p.add(jta1,BorderLayout.CENTER);
-        this.setTitle("最简单的聊天室-----antking");
-        this.add(p);
-        this.setSize(500,500);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
-        button.addActionListener(new buttonListener());
-	button2.addActionListener(new buttonListener2());
-        button3.addActionListener(new buttonListener3());
-   
-        client = new Client("localhost",54321);
-        while(true){
-	   int flag = client.input();
-	   if (flag == 1) {
-		jta1.append("chatroom:"+client.getMessage()+"\n");
-	   } else if (flag == 2) {
-		//System.out.println(client.getSender()+client.getmessage());
-		jta1.append("p2p:"+client.getSender()+client.getMessage()+"\n");
-	   } else if (flag == 3) {
-                ArrayList<String> userlist = client.getUserlist();
-                for(String str:userlist){
-                   jta1.append("userlist:"+str+"\n");
-                }
-           }
-           //jta1.append(client.input()+"\n");
-        }
-    }
-
-     private class buttonListener implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String str = jtf1.getText().toString();
-            client.output_to_chatroom(str);
-        }
-     
-    }
-
-    private class buttonListener2 implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String user = jtf2.getText().toString();
-	    String str = jtf1.getText().toString();
-            client.output_to_p2p(user, str);
-            jta1.append("p2p:me:"+str+"\n");
-        }
-     
-    }
-    private class buttonListener3 implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            client.requestUserlist();
-        }
-     
-    }
-    public static void main(String[] args) {
-        new newClient();
     }
 }
